96f1f05bf71ff108f6c47f46a20523bf5fefd122
jtkoch <jtkoch12@gmail.com>
2021-02-21 17:34:30 -0600

    changed a project

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 
 import traderjoes from '../images/traderjoes.png'
-import tetris from '../images/tetris.png'
+import benetic from '../images/benetic.png'
 import sailboat from '../images/sailboat.png'
 import tarot from '../images/tarot.png'
 import gol from '../images/gol.png'
@@ -18,11 +18,11 @@ class Carousel extends React.Component {
             items: [
                 {
                     id: 0,
-                    title: 'Trader Joe\'s Shopping List',
-                    subTitle: 'A interactive shopping list with my favorite items from TJ\'s that allows you to add items to the cart built with React and Context API',
-                    imgSrc: traderjoes,
-                    code: 'https://github.com/jtkoch/trader-joes-frontend',
-                    link: 'https://trader-joes-frontend.vercel.app/',
+                    title: 'Benetic',
+                    subTitle: 'This is an application built with Gatsby, Bootstrap, and Netlify functions. I worked from professionally designed wireframes to turn this into a fully functional website.',
+                    imgSrc: benetic,
+                    code: 'https://github.com/jtkoch',
+                    link: 'https://benetic.netlify.app/',
                     selected: false
                 },
                 {
@@ -45,6 +45,15 @@ class Carousel extends React.Component {
                 },
                 {
                     id: 3,
+                    title: 'Trader Joe\'s Shopping List',
+                    subTitle: 'A interactive shopping list with my favorite items from TJ\'s that allows you to add items to the cart built with React and Context API',
+                    imgSrc: traderjoes,
+                    code: 'https://github.com/jtkoch/trader-joes-frontend',
+                    link: 'https://trader-joes-frontend.vercel.app/',
+                    selected: false
+                },
+                {
+                    id: 4,
                     title: 'Away At Last',
                     subTitle: 'A website for Away At Last Travel & Tours built with HTML, CSS, JavaScript',
                     imgSrc: sailboat,
@@ -52,38 +61,14 @@ class Carousel extends React.Component {
                     link: 'https://www.awayatlasttravel.com/',
                     selected: false
                 },
-                {
-                    id: 4,
-                    title: 'Tetris',
-                    subTitle: 'The classic game of tetris built with JavaScript',
-                    imgSrc: tetris,
-                    code: 'https://github.com/jtkoch/tetris-app',
-                    link: 'https://sleepy-morse-844912.netlify.app/',
-                    selected: false
-                },
             ]
         }
     }
 
-    handleCardClick = (id, card) => {
-        let items = [...this.state.items]
-
-        items[id].selected = items[id].selected ? false : true
-
-        items.forEach(item => {
-            if(item.id !== id) {
-                item.selected = false
-            }
-        })
-
-        this.setState({
-            items
-        })
-    }
 
     makeItems = (items) => {
         return items.map(item => {
-            return <Card item={item} click={(e => this.handleCardClick(item.id, e))} key={item.id} />
+            return <Card item={item} key={item.id} />
         })
     }
 
