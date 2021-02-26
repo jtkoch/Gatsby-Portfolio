184bd1bd2bedf11225a09c34370eb8157ab1781d
jtkoch <jtkoch12@gmail.com>
2021-02-26 14:22:33 -0600

    trying a conditional to avoid the build error

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -3,13 +3,17 @@ import { useState } from "react"
 export const useLocalStorage = (key, initialValue) => {
 
   const [storedValue, setStoredValue] = useState(() => {
-      const item = localStorage.getItem(key)
+    if (typeof window !== 'undefined') {
+      const item = window.localStorage.getItem(key)
       return item ? JSON.parse(item) : initialValue
+    } else {
+      return null
+    }
   })
 
   const setValue = value => {
     setStoredValue(value)
-    localStorage.setItem(key, JSON.stringify(value))
+    window.localStorage.setItem(key, JSON.stringify(value))
   }
   return [storedValue, setValue]
 }
