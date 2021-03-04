19751a48108256eae5fa5ccef035f6dd5de78ed0
jtkoch <jtkoch12@gmail.com>
2021-03-04 12:45:07 -0600

    added thank you page after form submission

---
+++ src/pages/thanks.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+export default () => (
+  <Layout>
+    <div className="thanks-container">
+        <h1>Thanks for reaching out!</h1>
+        <p>I will get back to you as soon as possible.</p>
+    </div>
+  </Layout>
+)
